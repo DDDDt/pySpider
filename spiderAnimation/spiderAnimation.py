76cdc645915aa5943606b9ee084e7f80a105898d
tao.dai <tao.dai@wehotelglobal.com>
2018-11-26 13:14:42 +0800

    增加获取相关信息

--- spiderAnimation/spiderAnimation.py
+++ spiderAnimation/spiderAnimation.py
@@ -1,4 +1,5 @@
 import requests
+from pyquery import PyQuery as pq
 # 爬取动漫花园
 class SpiderAnimation(object):
     ## 查询
@@ -8,6 +9,25 @@ class SpiderAnimation(object):
             return req.text
         else:
             return None
+    
+    def getResultHeml(self,trPq: pq) -> (str,str,str,str,str,str,str,str,str):
+        date = trPq('td:first span').text().split()
+        dataType = trPq('td:eq(1) font').text().split()
+        dataName = trPq('td:eq(2) a').text().split()
+        dataUrl = trPq('td:eq(3) a').attr('href')
+        dataSize = trPq('td:eq(4)').text().split()
+        dataTor = trPq('td:eq(5)').text().split()
+        dataDow = trPq('td:eq(6) span').text().split()
+        dataCom = trPq('td:eq(7) a').text().split()
+        dataCreater = trPq('td:eq(8) a').text().split()
+        return (date,dataType,dataName,dataUrl,dataSize,dataTor,dataDow,dataCom,dataCreater)
+
+    def getRegexHtml(self,html: str) -> list:
+        pqHtml = pq(html)
+        tabPq = pqHtml('#topic_list')
+        rePq = tabPq('tbody tr')
+        res = rePq.map(lambda i,e : self.getResultHeml(pq(e)))
+        print(res.text)
 
 if __name__ == "__main__":
     headers = {
@@ -15,5 +35,6 @@ if __name__ == "__main__":
     }
     spiderAnimation = SpiderAnimation()
     html = spiderAnimation.getHtml("https://www.dmhy.org/topics/list/page/1")
-    print(html)
+    spiderAnimation.getRegexHtml(html)
+    # print(html)
         

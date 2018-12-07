3268a9a598fbb533e9a4869dc990a72a3813650d
tao.dai <tao.dai@wehotelglobal.com>
2018-12-07 18:56:47 +0800

    提交爬虫

--- spiderAnimation/spiderAnimation.py
+++ spiderAnimation/spiderAnimation.py
@@ -1,11 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
 import requests
 from pyquery import PyQuery as pq
 from pymongo import MongoClient
 import hashlib
+from pymongo import errors
+
 
 # 爬取动漫花园的数据类, 用语保存数据信息
-class DmhyInfo(object):
-
+class DmhyInfo:
     """
         id -> 用作 mongodb id
         date -> 日期
@@ -19,8 +22,8 @@ class DmhyInfo(object):
         dataCreater -> 发布人
         具体结构查看动漫花园表结构 -> http://share.dmhy.org/
     """
-    def __init__(self,id,date,dataType,dataName,dataUrl,dataSize,dataTor,dataDow,dataCom,dataCreater):
-        self._id = id
+    def __init__(self,_id: str,date: str,dataType: str,dataName: str,dataUrl: str,dataSize: str,dataTor: str,dataDow: str,dataCom: str,dataCreater:str):
+        self._id = _id
         self.date = date
         self.dataType = dataType
         self.dataName = dataName
@@ -30,18 +33,31 @@ class DmhyInfo(object):
         self.dataDow = dataDow
         self.dataCom = dataCom
         self.dataCreater = dataCreater
+
+headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+
 # 读取的数据存入 mongodb
-class addMongo(object):
+class AddMongo(object):
     def getCon(self):
-        client = MongoClient('mongodb://:27017/')
+        client = MongoClient('mongodb://root:@:27017/admin')
         return client
     # 插入 相关信息
-    def addMongoInfo(self):
+    def addMongoInfo(self,dmhyInfo: DmhyInfo) -> bool:
+        bol = True
         client = self.getCon()
-        db = client.pySpider
-        col = db.animation_info
-
-        
+        try:
+            db = client.pySpider
+            col = db.animation_info
+            col.insert_one(dmhyInfo.__dict__)
+        # 通过 id 判断有无重复
+        except errors.DuplicateKeyError as e:
+            print(e)
+            bol = False
+        finally:
+            client.close()
+            return bol
 
 # 爬取动漫花园
 class SpiderAnimation(object):
@@ -51,24 +67,38 @@ class SpiderAnimation(object):
         if req.status_code == 200:
             return req.text
         else:
+            print(req)
             return None
     
     def getResultHeml(self,trPq: pq) -> DmhyInfo:
         date = trPq('td:first span').text()
-        print(date)
-        dataType = trPq('td:eq(1) font').text().split()
-        dataName = trPq('td:eq(2) a').text().split()
+        dataType = trPq('td:eq(1) font').text()
+        dataName = trPq('td:eq(2) a').text()
         dataUrl = trPq('td:eq(3) a').attr('href')
-        dataSize = trPq('td:eq(4)').text().split()
-        dataTor = trPq('td:eq(5)').text().split()
-        dataDow = trPq('td:eq(6) span').text().split()
-        dataCom = trPq('td:eq(7) a').text().split()
-        dataCreater = trPq('td:eq(8) a').text().split()
+        if not dataUrl:
+            url = dataUrl = trPq('td:eq(2) a').attr('href')
+            dataUrl = self.getMagnet("https://www.dmhy.org/"+url)
+        dataSize = trPq('td:eq(4)').text()
+        dataTor = trPq('td:eq(5)').text()
+        dataDow = trPq('td:eq(6) span').text()
+        dataCom = trPq('td:eq(7) a').text()
+        dataCreater = trPq('td:eq(8) a').text()
         md = hashlib.md5()
-        md.update(dataName+dataUrl)
-        _id = md.digest()
+        md.update((','.join(dataName)+dataUrl).encode('utf-8'))
+        _id = md.hexdigest()
         return DmhyInfo(_id,date,dataType,dataName,dataUrl,dataSize,dataTor,dataDow,dataCom,dataCreater)
 
+    # 得到磁力链接    
+    def getMagnet(self,url: str) -> str:
+        html = self.getHtml(url,**headers)
+        if not html:
+            print("未获取磁力链接")
+            return None
+        pqHtml = pq(html)
+        magnet = pqHtml("#a_magnet").text()
+        return magnet
+
+    # 获取爬虫爬取的相关信息
     def getRegexHtml(self,html: str) -> list:
         pqHtml = pq(html)
         tabPq = pqHtml('#topic_list')
@@ -77,10 +107,21 @@ class SpiderAnimation(object):
         return res
 
 if __name__ == "__main__":
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-    }
     spiderAnimation = SpiderAnimation()
-    html = spiderAnimation.getHtml("https://www.dmhy.org/topics/list/page/1")
-    result = spiderAnimation.getRegexHtml(html)
-        
+    pageNums = range(1,4448)
+    for pageNum in reversed(pageNums):
+        html = spiderAnimation.getHtml("https://www.dmhy.org/topics/list/page/{0}".format(pageNum),**headers)
+        if not html:
+            print("https://www.dmhy.org/topics/list/page/{0} -> 为空".format(pageNum))
+            break
+        result = spiderAnimation.getRegexHtml(html)
+        if not result:
+            print("{0}为空".format(pageNum))
+        else:
+            mongo = AddMongo()
+            for i in result:
+                bol = mongo.addMongoInfo(i)
+                if not bol:
+                    break
+        print("完成 -> {0}".format(pageNum))
+            
